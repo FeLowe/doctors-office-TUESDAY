9e1d29f30f5af3cf140cdf973aa713cc0c15188d
Fernanda Lowe and Patrick Wells <fernanda@>
2016-05-03 16:23:03 -0700

    create class and methods for Doctors Office projetc

---
+++ src/main/java/Doctor.java
@@ -0,0 +1,64 @@
+import org.sql2o.*;
+import java.util.List;
+
+public class Doctor {
+  private String name;
+  private String specialty;
+  private int id;
+
+  public Doctor (String name, String specialty){
+  this.name = name;
+  this.specialty = specialty;
+  }
+
+  public String getName(){
+    return name;
+  }
+  public String getSpecialty(){
+    return specialty;
+  }
+  public int getId(){
+    return id;
+  }
+
+  public void save(){
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO doctors (name, specialty) VALUES (:name, :specialty)";
+      this.id = (int) con.createQuery(sql, true).addParameter("name", this.name).addParameter("specialty", this.specialty).executeUpdate().getKey();
+    }
+  }
+  public static List<Doctor> all(){
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT id, name, specialty FROM doctors";
+      return con.createQuery(sql).executeAndFetch(Doctor.class);
+    }
+  }
+  @Override
+  public boolean equals(Object otherDoctor) {
+    if (!(otherDoctor instanceof Doctor)) {
+      return false;
+    } else {
+      Doctor newDoctor = (Doctor) otherDoctor;
+      return this.getName().equals(newDoctor.getName()) &&
+      this.getSpecialty().equals(newDoctor.getSpecialty()) && this.getId() == newDoctor.getId();
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/test/java/DoctorTest.java
@@ -0,0 +1,67 @@
+import org.sql2o.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.List;
+
+public class DoctorTest{
+
+  @Before
+  public void setUp() {
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/doctors_office_test", null, null);
+  }
+
+  @After
+  public void tearDown() {
+    try(Connection con = DB.sql2o.open()) {
+      String deleteDoctors = "DELETE FROM doctors *;";
+      con.createQuery(deleteDoctors).executeUpdate();
+    }
+  }
+
+  @Test
+  public void doctor_instantiates_newDoctor(){
+    Doctor testDoctor = new Doctor("Carlos", "Gino");
+    assertTrue(testDoctor instanceof Doctor);
+  }
+
+  @Test
+  public void doctor_instantiates_getDoctorName(){
+    Doctor testDoctor = new Doctor("Carlos", "Gino");
+    assertEquals("Carlos" , testDoctor.getName());
+  }
+
+  @Test
+  public void doctor_instantiates_getDoctorSpecialty(){
+    Doctor testDoctor = new Doctor("Carlos", "Gino");
+    assertEquals("Gino", testDoctor.getSpecialty());
+  }
+
+  @Test
+  public void doctor_saveDoctorIntoDatabase(){
+    Doctor testDoctor = new Doctor("Carlos", "Gino");
+    testDoctor.save();
+    assertEquals(Doctor.all().size(), 1);
+  }
+
+  @Test
+  public void doctor_TestIfDoctorInstancesAreTheSame(){
+    Doctor testDoctor = new Doctor("Carlos", "Gino");
+    testDoctor.save();
+    assertTrue(Doctor.all().get(0).equals(testDoctor));
+  }
+
+  @Test
+  public void doctor_instantiates_getDoctorId(){
+    Doctor testDoctor = new Doctor("Carlos", "Gino");
+    testDoctor.save();
+    Doctor databaseDoctor = Doctor.all().get(0);
+    assertEquals(testDoctor.getId(), databaseDoctor.getId());
+  }
+
+
+
+
+
+
+
+}
